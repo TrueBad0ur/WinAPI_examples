ce505dacfae7b9ab4296469b0f518d133953d7e0
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2021-10-10 03:31:55 +0300

    Add EnumSystemProcesses function

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,7 @@
 #include <tchar.h> 
 #include <stdio.h>
 #include <strsafe.h>
+#include <psapi.h>
 
 void listFiles() {
     HANDLE hFind = INVALID_HANDLE_VALUE;
@@ -56,11 +57,46 @@ void dynamicDLLImport() {
     MessageBeepF(0xFFFFFFFF);
 }
 
+// function helper for EnumSystemProcesses()
+void PrintProcessNameAndID(DWORD processID) {
+    TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+
+    if (NULL != hProcess) {
+        HMODULE hMod;
+        DWORD cbNeeded;
+
+        if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded)) {
+            GetModuleBaseName(hProcess, hMod, szProcessName, sizeof(szProcessName) / sizeof(TCHAR));
+        }
+    }
+    _tprintf(TEXT("%s  (PID: %u)\n"), szProcessName, processID);
+    CloseHandle(hProcess);
+}
+
+int EnumSystemProcesses() {
+    DWORD aProcesses[1024], cbNeeded, cProcesses;
+    unsigned int i;
+
+    if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded)) {
+        return 1;
+    }
+
+    cProcesses = cbNeeded / sizeof(DWORD);
+    for (i = 0; i < cProcesses; i++) {
+        if (aProcesses[i] != 0) {
+            PrintProcessNameAndID(aProcesses[i]);
+        }
+    }
+}
+
 int main() {
 	listFiles();
     showWindow();
     infoAboutSystem();
     notepadExample();
     dynamicDLLImport();
+    // need higher rights
+    EnumSystemProcesses();
 	return 0;
 }
